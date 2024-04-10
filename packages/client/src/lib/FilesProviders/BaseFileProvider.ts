b82100e2b29397457ef7a2468fc7b13e73548f32
Samuele Girgenti <samuele.girgenti458@gmail.com>
2024-04-10 14:41:49 +0200

    Add HttpFileProvider for fetching assets

---
+++ packages/client/src/lib/FilesProviders/BaseFileProvider.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * Base class for file providers
+ */
+export abstract class BaseFileProvider {
+  constructor() {}
+
+  public async fetchResource(_path: string): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+}
--- packages/client/src/lib/index.ts
+++ packages/client/src/lib/index.ts
@@ -2,3 +2,4 @@ export * from "./AssetsClient";
 export * from "./ItemConfiguration";
 export * from "./TraitConfiguration";
 export * from "./AssetsClientConsumer";
+export * from "./FilesProviders";
