4bcfa13803fef1af5b8ebfc9c6ac2cf059c29397
Samuele Girgenti <samuele.girgenti458@gmail.com>
2024-04-14 00:40:58 +0200

    Update documentation build process

--- packages/client/scripts/postDocsGen.js
+++ packages/client/scripts/postDocsGen.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+const replace = require('replace-in-file');
 
 async function run() {
   // Checks if docs folder exists
@@ -17,9 +17,22 @@ async function run() {
 
   // Rename modules.md into README.md
   const modulesPath = path.resolve(docsPath, 'modules.md');
-  const readmePathNew = path.resolve(docsPath, 'README.md');
+  const readmePathNew = path.resolve(docsPath, 'Assets Client.md');
   if (fs.existsSync(modulesPath)) {
     fs.renameSync(modulesPath, readmePathNew);
+  }
+
+  // Replace 'modules.md' with 'Assets Client.md' in all files
+  const options = {
+    files: path.resolve(docsPath, "**", '*.md'),
+    from: /modules.md/g,
+    to: 'Assets Client.md',
+  };
+
+  try {
+    await replace(options);
+  } catch (error) {
+    console.error('Error occurred:', error);
   }
 
 }
